a182d65c2ca5fc7512123b869a24ebc9bc760ec2
Trelawney Edwards <s0306950@nescot.ac.uk>
2022-09-29 14:19:21 +0100

    Add project files.

---
+++ CurrencyConverter/default.aspx
@@ -0,0 +1,32 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="default.aspx.cs" Inherits="CurrencyConverter._default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <strong>Currency Converter<br />
+        <br />
+        </strong>
+        <asp:TextBox ID="valueTextBox" runat="server" AutoPostBack="True" OnTextChanged="valueTextBox_TextChanged"></asp:TextBox>
+        <br />
+        <br />
+        <br />
+        <asp:RadioButton ID="euroRadio" runat="server" AutoPostBack="True" GroupName="fromGroup" OnCheckedChanged="euroRadio_CheckedChanged" Text="Euro" />
+        <br />
+        <asp:RadioButton ID="dollarRadio" runat="server" AutoPostBack="True" GroupName="fromGroup" OnCheckedChanged="dollarRadio_CheckedChanged" Text="US Dollar" />
+        <br />
+        <asp:RadioButton ID="randRadio" runat="server" AutoPostBack="True" GroupName="fromGroup" OnCheckedChanged="randRadio_CheckedChanged" Text="Rand" />
+        <br />
+        <asp:RadioButton ID="kundRadio" runat="server" AutoPostBack="True" GroupName="fromGroup" OnCheckedChanged="kundRadio_CheckedChanged" Text="Kuna" />
+        <br />
+        <br />
+        <asp:Label ID="resultLabel" runat="server"></asp:Label>
+        <br />
+        <br />
+    </form>
+</body>
+</html>
---
+++ CurrencyConverter/default.aspx.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CurrencyConverter
+{
+    public partial class _default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void calculateResult(double exchangeRate)
+        {
+            if (valueTextBox.Text.Trim().Length == 0)
+                return;
+
+            double value = 0.0;
+            if (!Double.TryParse(valueTextBox.Text, out value))
+                return;
+            
+                double result = 0.0;
+
+            
+            result = Math.Round(value / exchangeRate, 2);
+
+            resultLabel.Text = "Value in UK £" + result.ToString();
+        
+        }
+
+       
+
+        protected void dollarRadio_CheckedChanged(object sender, EventArgs e)
+        {
+            calculateResult(1.32);
+        }
+
+        protected void kundRadio_CheckedChanged(object sender, EventArgs e)
+        {
+            calculateResult(6.47);
+        }
+
+        protected void randRadio_CheckedChanged(object sender, EventArgs e)
+        {
+            calculateResult(19.65);
+        }
+
+        protected void euroRadio_CheckedChanged(object sender, EventArgs e)
+        {
+            calculateResult(1.14);
+        }
+
+        protected void valueTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
